6966c1c2cb018b376c666a40d46ce3ab71948ebc
Roopaksh <72032743+Roopaksh1@users.noreply.github.com>
2023-07-12 18:38:47 +0530

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 const Footer = () => {
-
-}
+  return (
+    <div className="footer">
+      Copyright &copy; 2023 Roopaksh{' '}
+      <a href="https://github.com/Roopaksh1" target="_blank" rel="noreferrer">
+        <i className="fa-brands fa-github fa-lg"></i>
+      </a>
+    </div>
+  );
+};
 
 export default Footer;
